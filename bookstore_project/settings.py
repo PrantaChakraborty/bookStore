9dbdf643633ce78a1d40920449691b39d308f987
pranta <cbpranta@gmail.com>
2020-06-22 22:47:12 +0600

    ch9

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -158,4 +158,5 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'admin@prantaDjango.com'
