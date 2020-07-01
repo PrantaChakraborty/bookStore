60004ef3c19d42f983c06d07709353d09c3f1cca
pranta <cbpranta@gmail.com>
2020-07-01 11:32:39 +0600

    secure_proxy_ssl_header and allowed_hosts_update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['obscure-temple-91307.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -202,6 +202,7 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO', 'https')
 
 # heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
